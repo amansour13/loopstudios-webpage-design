119f1969463a22ca425812e330f817296ab71420
amansour13 <a.mansour.dev1@gmail.com>
2023-04-29 19:56:36 +0300

    initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='header'>
+      <div className='subHeader'>
+          <nav>
+            <h2>loopstudios</h2>
+            <ul>
+              <li><a href='#' className='pages'>About</a></li>
+              <li><a href='#' className='pages'>Careers</a></li>
+              <li><a href='#' className='pages'>Events</a></li>
+              <li><a href='#' className='pages'>Products</a></li>
+              <li><a href='#' className='pages'>Support</a></li>
+            </ul>
+          </nav>
+      </div>
+    </div>
+  );
+}
+
+export default App;
